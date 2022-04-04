66b8c4c5ea28500dfa949270b418d2de248e7fe3
Mihail <41691809+Koallla@users.noreply.github.com>
2022-04-04 13:01:48 +0300

    fixed comments

--- mysite/frontend/src/index.js
+++ mysite/frontend/src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-// import * as ReactDOMClient from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -19,19 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3%o$t+ggsm8-co+d)ss2)f)d%hyb#d5z06qw+1q$a4$=%a(##h'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# ALLOWED_HOSTS = localhost 127.0.0.1 [::1]
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-# 'DJANGO_ALLOWED_HOSTS' должен быть в виде одной строки с хостами разделенными символом пробела
-# Для примера: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
@@ -148,12 +137,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     )
-# }
 
 CORS_ORIGIN_ALLOW_ALL = False
 
